b9c30fde6936a434e3afc173f6f390150be41ef9
Diego Cantoral <diegocg1818@gmail.com>
2024-08-05 14:11:52 -0600

    Inicia

---
+++ Ejemplo/test.r
@@ -0,0 +1,24 @@
+# Asinación a la variable "x"
+c(10.4, 5.6, 3.1, 6.4, 21.7) -> x
+
+# Función para promedio
+promedio = mean(x)
+
+# Función para la varianza
+varianza = var(x)
+
+# La raíz cuadrada de la varianza es la desviación estándar
+desviacion_estandar = sqrt(var(x))
+
+# Tamaño del vector "x"
+n = length(x)
+
+y = x
+
+v <- 2 * x + y + 1
+
+c(10.4, 5.3, 5.7, 21.7) -> x
+
+seq(-5, 5, by = .1) -> s3
+
+hist(s3)
---
+++ R_guia.r
@@ -0,0 +1,14 @@
+setwd("ruta/directorio")  # Cambia de directorio
+getwd() # Obtiene la ruta actual
+dir() # Obtiene todos los documentos que están dentro
+mean(x) # promedio de un vector
+x <- c(10.4, 5.3, 5.7) # Asignar un vector a "x"
+assign("x", c(10.4, 5.3, 5.7)) # Asignar de otra manera
+c(10.4, 5.3, 5.7)  -> d # Asignación hacia la derecha
+y <- c(x, 0, x)
+
+min(x)
+max(x)
+sort(x)
+var(x) # varianza
+sqrt(var(x)) # desviación estándar
